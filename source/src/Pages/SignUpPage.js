1329157fb0656e1a79b5e2cf8e11833db3d0e05f
Max <bazarsadtsogtjargal@gmail.com>
2022-01-26 23:59:34 +0900

    form interaction

--- source/src/Pages/SignUpPage.js
+++ source/src/Pages/SignUpPage.js
@@ -1,4 +1,22 @@
+import { useState } from 'react';
+
 function SignUpPage() {
+  const [disabled, setDisabled] = useState(true);
+  const [password, setPassword] = useState('');
+  const [passwordRepeat, setPasswordRepeat] = useState('');
+
+  const onChangePassword = (event) => {
+    const inputPassword = event.target.value;
+    setPassword(inputPassword);
+    setDisabled(inputPassword !== passwordRepeat);
+  };
+
+  const onChangePasswordRepeact = (event) => {
+    const inputPassword = event.target.value;
+    setPasswordRepeat(inputPassword);
+    setDisabled(inputPassword !== password);
+  };
+
   return (
     <div>
       <h1>Sign Up</h1>
@@ -7,10 +25,10 @@ function SignUpPage() {
       <label htmlFor="email">E-mail</label>
       <input id="email" />
       <label htmlFor="password">Password</label>
-      <input id="password" type="password" />
+      <input id="password" type="password" onChange={onChangePassword} />
       <label htmlFor="password">Password Repeat</label>
-      <input id="password" type="password" />
-      <button disabled>Sign Up</button>
+      <input id="password" type="password" onChange={onChangePasswordRepeact} />
+      <button disabled={disabled}>Sign Up</button>
     </div>
   );
 }
--- source/src/Pages/SignUpPage.spec.js
+++ source/src/Pages/SignUpPage.spec.js
@@ -1,5 +1,6 @@
 import SignUpPage from './SignUpPage';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 describe('Sign Up Page', () => {
   describe('Layout', () => {
@@ -47,6 +48,17 @@ describe('Sign Up Page', () => {
       render(<SignUpPage />);
       const buton = screen.queryByRole('button', { name: 'Sign Up' });
       expect(buton).toBeDisabled();
+    });
+  });
+  describe('Interactions', () => {
+    it('enables button when passwords match', () => {
+      render(<SignUpPage />);
+      const passwordInput = screen.getByLabelText('Password');
+      const passwordRepeatInput = screen.getByLabelText('Password Repeat');
+      userEvent.type(passwordInput, 'P4assword');
+      userEvent.type(passwordRepeatInput, 'P4assword');
+      const button = screen.queryByRole('button', { name: 'Sign Up' });
+      expect(button).not.toBeEnabled();
     });
   });
 });
